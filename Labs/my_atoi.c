0d064f04ff1ff7fd3f217d77ea5c2caaebbd9ee6
vtian72 <39650677+vtian72@users.noreply.github.com>
2018-10-04 15:05:03 +0800

    Lab4

---
+++ Labs/my_atoi.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+int my_atoi(char string[]){
+    int i = 0;
+    int result = 0;
+    bool negative = false;
+
+    if(string[i] == '-'){
+      negative = true;
+      i++;
+    }
+    while(string[i] != '\0'){
+      result = result * 10 + (string[i] - '0');
+      i++;
+    }
+    if(negative){
+      return -result;
+    }
+    else{
+      return result;
+    }
+}
+
+int main(int argc, char *argv[]){
+    for(int i = 1; i < argc; i++){
+      printf("String conversion of %s is %i \n", argv[i], my_atoi(argv[i]));
+    }
+}
